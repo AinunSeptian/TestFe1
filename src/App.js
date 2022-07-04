df67f46213e50b5b39dc85ec00311d81296d50f0
AinunSeptian <ainunseptian79@gmail.com>
2022-07-04 13:43:28 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Select from 'react-select';
 
 function App() {
+
+  const [datas, setDatas] = useState([])
+  const [userSelect, setUserSelect] = useState("")
+
+  const handlerChange = (value) => {
+    setUserSelect(value)
+  }
+
+  const getBerries = async () => {
+    const berries = await fetch('https://pokeapi.co/api/v2/berry/');
+    const value = await berries.json()
+    const result = value.results.map((data) => {
+      return {
+        value: data.name,
+        label: data.name
+      }
+    })
+    const dataBerries = result.sort((a, b) => a.label.localeCompare(b.label))
+    setDatas(dataBerries)
+  }
+
+  useEffect(() => {
+    getBerries()
+  }, [])
+
   return (
     <div className="App">
-      <main>
-        <h1>Hallo World!</h1>
+      <main className='container'>
+        <h1>{userSelect}</h1>
+        <Select options={datas} onChange={(e) => handlerChange(e.value)} />
       </main>
     </div>
   );
